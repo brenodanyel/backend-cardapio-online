d1a5613b3109fab2d100d32a2dd2bd615170ca52
iDz <bdls1704@hotmail.com>
2023-04-04 21:56:08 -0300

    add category model

---
+++ src/database/models/category.model.ts
@@ -0,0 +1,15 @@
+import { model, Schema, Document } from 'mongoose';
+
+export interface Category {
+  parent: Category | null;
+  name: string;
+}
+
+export type CategoryDocument = Category & Document;
+
+export const CategorySchema = new Schema<Category>({
+  parent: { type: Schema.Types.ObjectId, ref: 'Category' },
+  name: { type: String, required: true },
+});
+
+export const CategoryModel = model<Category>('Category', CategorySchema);
